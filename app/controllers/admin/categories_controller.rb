beadd713a7bd8797572a4453f82934dfe051f72f
vagrant <seb.namek@gmail.com>
2017-11-09 16:40:53 +0000

    completed feature

--- app/controllers/admin/categories_controller.rb
+++ app/controllers/admin/categories_controller.rb
@@ -4,8 +4,24 @@ class Admin::CategoriesController < ApplicationController
   end
 
   def new
+    @category = Category.new
   end
 
   def create
+    @category = Category.new(product_params)
+
+    if @category.save
+      redirect_to [:admin, :categories], notice: 'Product created!'
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def product_params
+    params.require(:category).permit(
+      :name
+    )
   end
 end
